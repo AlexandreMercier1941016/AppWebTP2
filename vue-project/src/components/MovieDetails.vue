e847004eb7ef776dd970132d68a6d193fd132dac
AlexandreMercier1941016 <1941016@csfoy.ca>
2023-03-23 10:47:09 -0400

    movieDetails first interation

--- vue-project/src/components/MovieDetails.vue
+++ vue-project/src/components/MovieDetails.vue
@@ -1,16 +1,20 @@
+<script setup>
+    import MovieAPI from '@/services/MovieAPI.js';
+</script>
 <template>
     <div v-if="movie">
         <h2>{{movie.original_title}}</h2>
-        <img :src="movie.imageUrl ? movie.imageUrl : 'https://placeimg.com/200/200/tech'" width="200" style="float:right" />
-        <h3>{{product.description}}</h3>
-        <p>Price: {{product.price}}</p>
-        <p>Fixed price? {{product.fixedPrice}}</p>
-        <p>Discontinued? {{product.discontinued}}</p>
-        <p>Modified date: {{product.modifiedDate }}</p>
+        <img :src="movie.backdrop_path" width="200" style="float:right" />
+        <h3>{{movie.homepage}}</h3>
+        <p>Ratings: {{movie.vote_average}}</p>
+        <p>Length: {{movie.runtime}}</p>
+        <p>Released Date: {{movie.discontinued}}</p>
+        <p>Official Site: {{movie.homepage }}</p>
     </div>
 </template>
 
 <script>
+//67e6614f0a5371f8047de1bf746b514f
     export default {
         props: {
             movie: {
