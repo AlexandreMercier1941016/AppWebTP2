84a5e2fdfab1ac864a781eeff251085fee5c7596
Math Guerin <guerinm3@gmail.com>
2023-03-23 10:19:03 -0400

    homeView first iteration done

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <movie-list :products="products" :page-size="10"></movie-list>
+  </div>
+</template>
+
+<script>
+import MovieList from '@/components/MovieList.vue'
+import {getWelcomePageMovieList} from '@/service/fetchProductData.js'
+    export default {
+        name: 'App',
+  components: {
+    MovieList
+  },
+  data() {
+    return {
+        id: 1,
+        products: null
+    }
+    },
+    methods : {
+        async fetchData() {
+            const response = await getWelcomePageMovieList();
+            this.products = await response.JSON()
+        }
+    },
+    mounted () { // aura lieu au chargement du component
+        this.fetchData();
+    },
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
