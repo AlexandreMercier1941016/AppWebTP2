938824464ef8a95e107704f7514225192f739218
AlexandreMercier1941016 <1941016@csfoy.ca>
2023-03-23 10:18:57 -0400

    ajout de movieDetails

---
+++ vue-project/src/components/MovieDetails.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="movie">
+        <h2>{{movie.original_title}}</h2>
+        <img :src="movie.imageUrl ? movie.imageUrl : 'https://placeimg.com/200/200/tech'" width="200" style="float:right" />
+        <h3>{{product.description}}</h3>
+        <p>Price: {{product.price}}</p>
+        <p>Fixed price? {{product.fixedPrice}}</p>
+        <p>Discontinued? {{product.discontinued}}</p>
+        <p>Modified date: {{product.modifiedDate }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            movie: {
+                type: Object,
+                default: null
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
