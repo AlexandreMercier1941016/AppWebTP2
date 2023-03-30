b3b268b68464bf19e7db38d9becd6778aac90928
AlexandreMercier1941016 <1941016@csfoy.ca>
2023-03-30 10:17:17 -0400

    ajout route for movieImage

--- vue-project/src/services/MovieAPI.js
+++ vue-project/src/services/MovieAPI.js
@@ -5,6 +5,8 @@ let headers = { Accept: "application/json", "Content-Type": "application/json"};
 const RESOURCE_NAME_SINGLE_MOVIE = "movie/";
 const RESOURCE_NAME_UPCOMING = "movie/upcoming";
 const RESOURCE_NAME_SEARCH = "search/movie";
+const RESOURCE_NAME_IMAGE = "/images";
+//https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US
 
 export async function getSingleMovie(id) {
     const response = await fetch(baseURL + RESOURCE_NAME_SINGLE_MOVIE + id + apiKey, headers);
@@ -16,6 +18,10 @@ export async function getWelcomePageMovies() {
 }
 export async function getFilteredMoviesByKeyWords(query) {
     const response = await fetch(baseURL + RESOURCE_NAME_SEARCH + apiKey + query, headers);
+    return await response.json();
+}
+export async function getMovieImage(id) {
+    const response = await fetch(baseURL + "movie/" + id + RESOURCE_NAME_IMAGE + apiKey + query, headers);
     return await response.json();
 }
 export async function postAppreciation(id, appreciation) {
