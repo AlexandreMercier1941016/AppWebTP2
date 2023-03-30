cf7ae6c5c70a1cf19df985ede621071c034030ca
Math Guerin <guerinm3@gmail.com>
2023-03-30 10:20:22 -0400

    fix movieList to show movieName and movieDescription

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,7 +5,6 @@ import MovieList from './components/MovieList.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
 
