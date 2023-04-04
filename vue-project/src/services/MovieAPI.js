25c7ad6332604981dd3eac9e277df28cc62a4297
AlexandreMercier1941016 <1941016@csfoy.ca>
2023-04-04 14:33:49 -0400

    correction de la route postAppreciation and i added the guest session id const

--- vue-project/src/services/MovieAPI.js
+++ vue-project/src/services/MovieAPI.js
@@ -2,6 +2,7 @@ let baseURL = 'https://api.themoviedb.org/3/';
 let apiKey = "?api_key=67e6614f0a5371f8047de1bf746b514f&language=en-US"
 let apiKeyWithoutLanguage = "?api_key=67e6614f0a5371f8047de1bf746b514f"
 let headers = { Accept: "application/json", "Content-Type": "application/json"};
+const GUEST_SESSION_ID = fetch("https://api.themoviedb.org/3/authentication/guest_session/new" + apiKeyWithoutLanguage,headers).json().then(response => GUEST_SESSION_ID = response);
 
 const RESOURCE_NAME_SINGLE_MOVIE = "movie/";
 const RESOURCE_NAME_UPCOMING = "movie/upcoming";
@@ -17,8 +18,8 @@ export async function getWelcomePageMovies() {
     const response = await fetch(baseURL + RESOURCE_NAME_UPCOMING+ apiKey, headers);
     return await response.json();
 }
-export async function getFilteredMoviesByKeyWords(query) {
-    const response = await fetch(baseURL + RESOURCE_NAME_SEARCH + apiKeyWithoutLanguage + "&query=" + query, headers);
+export async function getFilteredMoviesByKeyWords(keywords,year,getGenres) {
+    const response = await fetch(baseURL + RESOURCE_NAME_SEARCH + apiKeyWithoutLanguage + "&query=" + query + "&year=" + year + "&genre=" + genre + "&page=0", headers);
     return await response.json();
 }
 export async function getMovieImage(id) {
@@ -30,10 +31,13 @@ export async function getGenres() {
     return await response.json();
 }
 export async function postAppreciation(id, appreciation) {
-    const response = await fetch(baseURL + RESOURCE_NAME_SEARCH + apiKey + query, headers);
-    fetch(baseURL+"movie/"+id+"/rating"+apiKey, {
+    fetch("https://api.themoviedb.org/3/movie/" + id + "/rating" + apiKeyWithoutLanguage + "&guest_session_id=" + GUEST_SESSION_ID, {
         method: 'POST',
         headers,
         body: JSON.stringify({ "value": appreciation })
     }).then(response => response.json())
+}
+
+export function getGuestSessionId() {
+    return GUEST_SESSION_ID;
 }
