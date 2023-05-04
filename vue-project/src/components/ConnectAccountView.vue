4835fca158a93d5f9aeedd8904332d1f46a8fc35
Math Guerin <guerinm3@gmail.com>
2023-05-04 08:57:33 -0400

    created login view and login component

---
+++ vue-project/src/components/ConnectAccountView.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <form id="rendered-form">
+            <div class="rendered-form">
+                <div class="formbuilder-text form-group field-username">
+                    <label for="username" class="formbuilder-text-label">Nom d'utilisateur
+                        <br>
+                        <span class="formbuilder-required">*</span>
+                    </label>
+                    <input type="email" class="form-control" name="username" maxlength="50" id="username" required="required" aria-required="true">
+                </div>
+                <div class="formbuilder-text form-group field-password">
+                    <label for="password" class="formbuilder-text-label">Mot de passe
+                        <br>
+                        <span class="formbuilder-required">*</span>
+                    </label>
+                    <input type="password" class="form-control" name="password" maxlength="50" id="password" required="required" aria-required="true">
+                </div>
+                <div class="formbuilder-button form-group field-button-1683049578105-0">
+                    <button @click="getTokenFromLogin()" type="button" class="btn-primary btn" name="button-1683049578105-0" style="primary" id="button-1683049578105-0">Se connecter
+                        <br>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import{getLoginToken}from '@/services/MovieAPI.js'
+    export default {
+        data(){
+             username:document.getElementById("username");
+             password:document.getElementById("password");
+        },
+        methods: {
+            getTokenFromLogin() {
+                getLoginToken(username,password)
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/views/LoginAccountView.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import ConnectAccountView from '../components/ConnectAccountView.vue';
+</script>
+<template>
+    <div>
+        <ConnectAccountView></ConnectAccountView>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
