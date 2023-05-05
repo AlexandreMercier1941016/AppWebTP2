f127c2683a149848bbf66824f2546c60894ad51b
Math Guerin <guerinm3@gmail.com>
2023-05-05 14:44:01 -0400

    login working

--- vue-project/src/components/ConnectAccountView.vue
+++ vue-project/src/components/ConnectAccountView.vue
@@ -1,5 +1,9 @@
+<script setup>
+import { useUserStore } from '../store/userStore.js';
+</script>
 <template>
     <div>
+        <div v-if="!disable">
         <form id="rendered-form">
             <div class="rendered-form">
                 <div class="formbuilder-text form-group field-username">
@@ -7,38 +11,53 @@
                         <br>
                         <span class="formbuilder-required">*</span>
                     </label>
-                    <input type="email" class="form-control" name="username" maxlength="50" id="username" required="required" aria-required="true">
+                    <input v-model="username" type="email" class="form-control" name="username" maxlength="50" id="username" required="required" aria-required="true">
                 </div>
                 <div class="formbuilder-text form-group field-password">
                     <label for="password" class="formbuilder-text-label">Mot de passe
                         <br>
                         <span class="formbuilder-required">*</span>
                     </label>
-                    <input type="password" class="form-control" name="password" maxlength="50" id="password" required="required" aria-required="true">
+                    <input v-model="password" type="password" class="form-control" name="password" maxlength="50" id="password" required="required" aria-required="true">
                 </div>
                 <div class="formbuilder-button form-group field-button-1683049578105-0">
-                    <button @click="getTokenFromLogin()" type="button" class="btn-primary btn" name="button-1683049578105-0" style="primary" id="button-1683049578105-0">Se connecter
+                    <button @click="getTokenFromLogin(username,password)" type="button" class="btn-primary btn" name="button-1683049578105-0" style="primary" id="button-1683049578105-0">Se connecter
                         <br>
                     </button>
                 </div>
             </div>
         </form>
+        </div>
+        {{errorMessage}}
     </div>
 </template>
 
 <script>
     import{getLoginToken}from '@/services/MovieAPI.js'
+    import router from '../router';
+    const store=useUserStore();
     export default {
         data(){
-             username:document.getElementById("username");
-             password:document.getElementById("password");
-        },
-        methods: {
-            getTokenFromLogin() {
-                getLoginToken(username,password)
+            return{
+            errorMessage:""
+            ,disable:false
             }
         }
-
+        ,methods: {
+            async getTokenFromLogin(username,password){
+                let token= await getLoginToken(username,password)
+                if(token==null||token==''){
+                    this.errorMessage="l'utilisateur ou le mot de passe est invalide"
+                    console.log("erreur lancé")
+                }else{
+                    store.setToken(token);
+                    store.setName(username);
+                    this.errorMessage="Connexion effectué avec succès!"
+                    this.disable=true;
+                    setTimeout(() => {  router.push({path:'movies'}); }, 4000);
+                }
+            }
+        }
     }
 </script>
 
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -27,6 +27,14 @@ const router = createRouter({
       component: () => import('../views/MovieListSearchResult.vue'),
       props: castRouteParams2
     },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue'),
+    },
   ]
 })
 
