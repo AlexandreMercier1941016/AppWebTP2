804f2a85d2b3fa48d43891f723b4a1199a0b0714
Math Guerin <guerinm3@gmail.com>
2023-05-17 17:02:29 -0400

    cleaned userStore from console.log

--- vue-project/src/store/userStore.js
+++ vue-project/src/store/userStore.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
 
 export const useUserStore = defineStore('user', {
-    state: () => ({ token: useLocalStorage('pinia/auth/login','user'), name: 'login' }),
+    state: () => ({ token: useLocalStorage('pinia/auth/login','user'),name: useLocalStorage('pinia/auth/login','username') }),
     getters: {
         getName: state => {
             return state.name
@@ -17,12 +17,11 @@ export const useUserStore = defineStore('user', {
   // Pour générer un id unique, on prend l'id le plus grand des items et on lui ajoute 1. S'il n'y a pas de items, on commence à 1.
   // Cette façon de faire pour générer un id convient dans cet exemple, mais ce n'est pas la façon de faire si l'application communique avec par exemple, un API REST. Dans ce cas, il serait préférable de laisser le serveur générer l'id.
         this.token=token
-        console.log(this.token)
     },
     setName (name) {
         // Pour générer un id unique, on prend l'id le plus grand des items et on lui ajoute 1. S'il n'y a pas de items, on commence à 1.
         // Cette façon de faire pour générer un id convient dans cet exemple, mais ce n'est pas la façon de faire si l'application communique avec par exemple, un API REST. Dans ce cas, il serait préférable de laisser le serveur générer l'id.
-              this.name=name
-          },
+        this.name=name
+    }
   }
 })
