b41f0ecc5d0bfe0270d6e1a49b9690ad30ffbff1
Alexandre Mercier <alex.mercier@live.ca>
2023-05-18 18:14:40 -0400

    test d'une ligne

--- vue-project/src/services/auth.js
+++ vue-project/src/services/auth.js
@@ -6,7 +6,8 @@ setup(){
     return { store }
  },
   async isUserAdmin(){
-    const user= await getUserInfo(this.store.getToken)
+    //const user= await getUserInfo(this.store.getToken)
+    const user= await getUserInfo(useUserStore().getToken)
     if(user.role_id==1){
       return true;
     }else{
